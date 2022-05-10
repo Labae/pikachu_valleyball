efc74d03904bbaaa80573949d7d4b1c5cd69b2a1
Sungsik Bae <46157657+Labae@users.noreply.github.com>
2022-05-10 15:49:19 +0900

    add rapier2d physics

--- src/components/mod.rs
+++ src/components/mod.rs
@@ -1,9 +1,3 @@
 pub use self::animation::Animation;
-pub use self::intersect_detector_2d::IntersectDetector2D;
-pub use self::primitives::Collider2D;
-pub use self::rigidbody2d::Rigidbody2D;
 
 mod animation;
-mod intersect_detector_2d;
-mod primitives;
-mod rigidbody2d;
